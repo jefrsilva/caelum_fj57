e5a7adb740aa0ae1c70e70df4d988cb605f9c628
jefrsilva <jefrsilva@gmail.com>
2014-11-21 13:42:31 -0200

    Ex.4.2 - Nossa classe de modelo e extraindo os dados do formulario

--- app/src/main/java/br/com/caelum/cadastrocaelum/FormularioActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/FormularioActivity.java
@@ -8,19 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import br.com.caelum.cadastrocaelum.modelo.Aluno;
+
 
 public class FormularioActivity extends Activity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulario);
 
+        this.helper = new FormularioHelper(this);
+
         Button botao = (Button) findViewById(R.id.botao);
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FormularioActivity.this, "Você clicou no botão", Toast.LENGTH_LONG).show();
+                Aluno aluno = helper.pegaAlunoDoFormulario();
+                Toast.makeText(FormularioActivity.this, "Aluno preenchido: " + aluno.getNome(), Toast.LENGTH_LONG).show();
                 finish();
             }
         });
---
+++ app/src/main/java/br/com/caelum/cadastrocaelum/FormularioHelper.java
@@ -0,0 +1,42 @@
+package br.com.caelum.cadastrocaelum;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import br.com.caelum.cadastrocaelum.modelo.Aluno;
+
+/**
+ * Created by jefrsilva on 21/11/14.
+ */
+public class FormularioHelper {
+    private Aluno aluno;
+    private final EditText nome;
+    private final EditText telefone;
+    private final EditText endereco;
+    private final EditText site;
+    private final SeekBar nota;
+    private final ImageView foto;
+
+    public FormularioHelper(FormularioActivity activity) {
+        this.aluno = new Aluno();
+
+        nome = (EditText) activity.findViewById(R.id.nome);
+        telefone = (EditText) activity.findViewById(R.id.telefone);
+        endereco = (EditText) activity.findViewById(R.id.endereco);
+        site = (EditText) activity.findViewById(R.id.site);
+        nota = (SeekBar) activity.findViewById(R.id.nota);
+        foto = (ImageView) activity.findViewById(R.id.foto);
+    }
+
+    public Aluno pegaAlunoDoFormulario() {
+        aluno.setNome(nome.getText().toString());
+        aluno.setTelefone(telefone.getText().toString());
+        aluno.setEndereco(endereco.getText().toString());
+        aluno.setSite(site.getText().toString());
+        aluno.setNota(Double.valueOf(nota.getProgress()));
+
+        return aluno;
+    }
+}
