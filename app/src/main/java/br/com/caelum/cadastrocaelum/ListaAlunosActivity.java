2e074a3f8b99bdec2da4e6b531b96966cc61e2ef
jefrsilva <jefrsilva@gmail.com>
2014-11-21 12:21:17 -0200

    Commit inicial.

---
+++ app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
@@ -0,0 +1,17 @@
+package br.com.caelum.cadastrocaelum;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ListaAlunosActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listagem_alunos);
+    }
+
+}
