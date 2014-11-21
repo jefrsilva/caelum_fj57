e2b97dd86403ba39fe5eadc5bb5e0f2cda6fcca6
jefrsilva <jefrsilva@gmail.com>
2014-11-21 13:01:39 -0200

    Ex.2.1 - Criando a lista de alunos

--- app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
@@ -4,14 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class ListaAlunosActivity extends Activity {
+
+    private ListView listaAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listagem_alunos);
+
+        String[] alunos = {"Anderson", "Filipe", "Guilherme"};
+        this.listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 
 }
