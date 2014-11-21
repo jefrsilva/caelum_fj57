60678469a6a3ff0f34b81095cac34d16d1e648e9
jefrsilva <jefrsilva@gmail.com>
2014-11-21 13:08:21 -0200

    Ex.2.3 - Integrando o Toast

--- app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class ListaAlunosActivity extends Activity {
@@ -21,6 +24,22 @@ public class ListaAlunosActivity extends Activity {
         this.listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View convertView, int posicao, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "Posição selecionada: " + posicao, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listaAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View convertView, int posicao, long id) {
+                String aluno = (String) adapterView.getItemAtPosition(posicao);
+                Toast.makeText(ListaAlunosActivity.this, "Clique longo: " + aluno, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 
 }
