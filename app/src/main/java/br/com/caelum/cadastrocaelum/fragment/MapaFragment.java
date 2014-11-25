7c42f1d7b748ccc4956370b2c26df27529a44478
jefrsilva <jefrsilva@gmail.com>
2014-11-25 12:42:16 -0200

    Ex.14.6 - Exibindo os alunos no mapa

--- app/src/main/java/br/com/caelum/cadastrocaelum/fragment/MapaFragment.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/fragment/MapaFragment.java
@@ -1,9 +1,47 @@
 package br.com.caelum.cadastrocaelum.fragment;
 
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import br.com.caelum.cadastrocaelum.dao.AlunoDAO;
+import br.com.caelum.cadastrocaelum.modelo.Aluno;
+import br.com.caelum.cadastrocaelum.util.Localizador;
 
 /**
  * Created by jefrsilva on 25/11/14.
  */
 public class MapaFragment extends SupportMapFragment {
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Localizador localizador = new Localizador(getActivity());
+        LatLng local = localizador.getCoordenada("Rua Vergueiro 3185, Vila Mariana, São Paulo");
+
+        centralizaNo(local);
+
+        AlunoDAO dao = new AlunoDAO(getActivity());
+        List<Aluno> alunos = dao.getLista();
+        dao.close();
+        for (Aluno aluno : alunos) {
+            local = localizador.getCoordenada(aluno.getEndereco());
+            MarkerOptions marcador = new MarkerOptions();
+            marcador.title(aluno.getNome()).position(local);
+            getMap().addMarker(marcador);
+        }
+    }
+
+    private void centralizaNo(LatLng local) {
+        GoogleMap mapa = getMap();
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(local, 17);
+        mapa.moveCamera(camera);
+    }
 }
---
+++ app/src/main/java/br/com/caelum/cadastrocaelum/util/Localizador.java
@@ -0,0 +1,36 @@
+package br.com.caelum.cadastrocaelum.util;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by jefrsilva on 25/11/14.
+ */
+public class Localizador {
+    private Geocoder geo;
+
+    public Localizador(Context context) {
+        geo = new Geocoder(context);
+    }
+
+    public LatLng getCoordenada(String endereco) {
+        try {
+            List<Address> listaEnderecos;
+            listaEnderecos = geo.getFromLocationName(endereco, 1);
+            if (!listaEnderecos.isEmpty()) {
+                Address address = listaEnderecos.get(0);
+                return new LatLng(address.getLatitude(), address.getLongitude());
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
